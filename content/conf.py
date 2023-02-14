5667ac8e6f42737c3aff16438c2f3c475da3a6a7
Björn Ricks <bjoern.ricks@greenbone.net>
2023-02-14 18:58:38 +0100

    Fix blog URL

--- content/conf.py
+++ content/conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Björn Ricks"  # (translatable)
 BLOG_TITLE = "Björn Ricks"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://bjoernricks.github.io /"
+SITE_URL = "https://bjoernricks.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
